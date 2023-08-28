a6036ba1aad7b663530bffad8b2e2c2c62c1a9a9
erickdaguilar <54286983+erickdaguilar@users.noreply.github.com>
2023-08-28 12:28:09 -0600

    Create fizzBuzz.py

---
+++ Dia1/fizzBuzz.py
@@ -0,0 +1,13 @@
+eNumero = 1
+
+deffizzBuzz(eNumero)
+	if eNumero %3 == 0:
+		print(fizz)
+	if eNumero %5 == 0
+		print(buzz)
+	else :
+		print(eNumero)
+
+while (eNumero <= 100)
+	eNumero += 1
+	fizzBuzz(eNumero)
